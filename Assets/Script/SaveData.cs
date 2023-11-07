eefcb43915edfb8a85c854cdafe6e6193cf14ccf
dia2794 <rlaskadn02@naver.com>
2023-11-07 20:33:32 +0900

    Clicker

--- Assets/Script/SaveData.cs
+++ Assets/Script/SaveData.cs
@@ -3,6 +3,8 @@ public class SaveData{//저장할 변수들
     public int gold;
     public int xp;
     public int level;
+    public int plants_lv;
+    public int meats_lv;
     public float slime_size;
     public float BGM_value;
     public float SFX_value;
